c8d1b9d2986827e7fc228fc994ef0487a1ec03fa
Victor Rodrigues <victor.rodrigues135@gmail.com>
2020-08-24 20:21:00 -0300

    mural

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
+
+const fs = require('fs');
 const express = require('express');
 const app = express();
 //View
-
+// function view(pagina) {
+//     return fs.readFileSync(__dirname+'/view/'+pagina+'.html', {encoding:'utf8', flag:'r'});
+// }
+let comentarios = [];
 //Controlador
 let muralController = {
-    index: (req, res) => res.send("Entrei no mural!")
+    index: (req, res) => {
+        let html_comentarios = comentarios.join("<br>");
+        res.send("Estou no meu mural<br>" + html_comentarios);
+    },
+    comentar: (req, res) => {
+        comentarios.push("Novo comentario!")
+        res.send("MURAL COMENTADO!");
+    }
 }
 
 //Rotas
 app.get("/mural", muralController.index);
+app.get("/comentario", muralController.comentar);
 
 
 // Inicia o servidor NODE
