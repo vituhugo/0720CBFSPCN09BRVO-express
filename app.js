906796ad030f312eed7c6f344cc85178c685d1d7
Victor Rodrigues <victor.rodrigues135@gmail.com>
2020-08-24 20:38:25 -0300

    mural

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-
-const fs = require('fs');
 const express = require('express');
 const app = express();
 //View
@@ -9,19 +7,27 @@ const app = express();
 let comentarios = [];
 //Controlador
 let muralController = {
+
     index: (req, res) => {
         let html_comentarios = comentarios.join("<br>");
         res.send("Estou no meu mural<br>" + html_comentarios);
     },
+
     comentar: (req, res) => {
         comentarios.push("Novo comentario!")
         res.send("MURAL COMENTADO!");
+    },
+
+    showComentario: (req, res) => {
+        let numero = req.params.numero;
+        res.send(comentarios[numero]);
     }
 }
 
 //Rotas
 app.get("/mural", muralController.index);
-app.get("/comentario", muralController.comentar);
+app.get("/mural/:numero", muralController.showComentario);
+app.get("/comentar", muralController.comentar);
 
 
 // Inicia o servidor NODE
